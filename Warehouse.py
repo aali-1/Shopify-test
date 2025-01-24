7a5e61eaff9da27d894a69b2a3f351c52040e761
AaliCharania <aali.charania@gmail.com>
2025-01-24 16:11:51 -0500

    ship within country

--- Warehouse.py
+++ Warehouse.py
@@ -1,3 +1,4 @@
+import math
 class Warehouse:
     def __init__(self, city, country, coordinates, stock):
         self.city = city
@@ -18,6 +19,22 @@ class Product:
             if warehouse.country == sale_info.buyer.country:
                 cities.append(warehouse.city)
         return cities
+
+    def euclidean_distance(self, x1, y1, x2, y2):
+        return math.sqrt((x2 - x1)**2 + (y2 - y1)**2)
+    def closest_to_buyer(self, sale_info):
+        if self.name != sale_info.product:
+            return []
+        min_distance = float('inf')
+        min_city = []
+        for warehouse in self.warehouses:
+            distance = self.euclidean_distance(sale_info.buyer.coordinates[0], sale_info.buyer.coordinates[1], warehouse.coordinates[0], warehouse.coordinates[1])
+            if distance < min_distance:
+                min_city = [warehouse.city]
+                min_distance = distance
+            
+        return min_city
+        
         
 
 class Buyer:
--- tests.py
+++ tests.py
@@ -2,12 +2,17 @@ from Warehouse import *
 
 laptop_warehouses = [Warehouse("Toronto", "Canada", [2,2], 1), 
                      Warehouse("Montreal", "Canada", [3,-1], 99), 
-                     Warehouse("Seattle", "USA", [1,1], 100)]
+                     Warehouse("Seattle", "USA", [-2,1], 100),
+                     Warehouse("London", "UK", [10,3], 1),]
 
 laptop = Product("Laptop", laptop_warehouses)
 
 tom = Buyer("Tom", "Vancouver", "Canada", [-2,5])
 kevin_usa = Buyer("Kevin", "Seattle", "USA", [1,1])
 john_uk = Buyer("John", "London", "UK", [0,0])
+jack_paris = Buyer("Jack", "Paris", "France", [15,-3])
+print("ship within country")
 print(laptop.ship_within_country(SaleInfo(tom, "Laptop", 1)))
 print(laptop.ship_within_country(SaleInfo(kevin_usa, "Laptop", 1)))
+print("ship closest to buyer")
+print(laptop.closest_to_buyer(SaleInfo(jack_paris, "Laptop", 1)))
